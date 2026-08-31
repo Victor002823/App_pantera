0bf75e314ea9f97db47a86b4ea75480c1cf5aac2
Tu Nombre <tu-correo@example.com>
2026-08-30 22:05:25 -0600

    Agregar CORS para recibir cotizaciones desde dashword

--- view/home/recibir_transferencia.php
+++ view/home/recibir_transferencia.php
@@ -1,5 +1,13 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: https://control.mudanzasellince.com');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 require_once(__DIR__ . "/../../config/db.php");
 
 $CLAVE_SECRETA = getenv('LINCE_TRANSFER_KEY') ?: '';
