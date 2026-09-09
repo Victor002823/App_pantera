969357fb34549a73abc87fcad8e62b533a8c1de2
Tu Nombre <tu-correo@example.com>
2026-09-09 17:51:18 -0600

    debug: agregar eruda temporal para diagnosticar boton huella

--- login.php
+++ login.php
@@ -65,6 +65,7 @@
     <span id="nombreUsuario">Asesor</span>
     <i class="fa-solid fa-location-dot" id="markerIcon" style="display:none; color:red;"></i>
 </h1>
+<script src="https://cdn.jsdelivr.net/npm/eruda"></script><script>eruda.init();</script>
 <script src="/view/home/funciones_login.js"></script>
 
 </body>
